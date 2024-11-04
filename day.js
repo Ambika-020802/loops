5a08d423d47329b219a9302072442e83fb9e4ca2
Ambika-020802 <ambikarekandar2002@gmail.com>
2024-11-04 23:46:57 +0530

    Add files via upload

---
+++ day.js
@@ -0,0 +1,100 @@
+
+//FOR LOOP
+
+for(var a=1;a<3;a++){
+    console.log(a);
+    
+}
+
+
+for (var a=10;a<=15;a++){
+    console.log(a);
+}
+
+
+for(var a=10;a>7;a--){
+    console.log(a);
+    
+}
+
+
+for(var i=20;i>=15;i--){
+    console.log(i);
+    
+}
+
+
+
+
+//WHILE LOOP
+
+var a="1";
+while(a<5){
+console.log(a);
+a++;
+}
+
+
+var a="3";
+while(a<5){
+    console.log(a);
+    a++;
+    
+}
+
+
+var a="10";
+while(a>6){
+    console.log(a);
+    a--;
+    
+}
+
+
+var a="20";
+while(a>=18){
+    console.log(a);
+    a--;
+    
+}
+
+
+// DO WHILE
+
+
+
+var a="1";
+do{ 
+    a++;
+    console.log(a);
+    
+}while(a<5);
+
+
+
+
+var a="10";
+do{
+    a--;
+    console.log(a);
+    
+}while(a>5);
+
+
+
+var a="10";
+// do{
+//     a--
+    console.log(a);
+    
+}while (a>=7)
+
+
+
+var a="10";
+do{
+    console.log(a);
+    a--;
+}while(a>=7);
+
+
